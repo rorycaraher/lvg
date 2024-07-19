c8fd21a8da2d7194e0653f95c4c50a21f589a7d3
Rory Caraher <caraher.rory@gmail.com>
2024-07-19 12:36:04 +0200

    Added Google Cloud SQL resources for a database instance, database, and user in Terraform configuration.

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,6 +2,27 @@ data "google_project" "project" {
   project_id = var.project
 }
 
+resource "google_sql_database_instance" "db_instance" {
+  name             = "${var.project}-db-instance"
+  database_version = "POSTGRES_13"
+  region           = var.region
+
+  settings {
+    tier = "db-f1-micro"
+  }
+}
+
+resource "google_sql_database" "db" {
+  name     = "level_values_db"
+  instance = google_sql_database_instance.db_instance.name
+}
+
+resource "google_sql_user" "db_user" {
+  name     = "db_user"
+  instance = google_sql_database_instance.db_instance.name
+  password = "your-password"  # Replace with a secure password
+}
+
 provider "google" {
   project = data.google_project.project.project_id
   region  = var.region
