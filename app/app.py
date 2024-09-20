42e894778684ebe348b6d6fa187b311c257f5d84
Rory Caraher <caraher.rory@gmail.com>
2024-09-20 12:15:17 +0200

    k8s init

--- app/app.py
+++ app/app.py
@@ -1,8 +1,6 @@
 from flask import Flask, request, jsonify, send_from_directory
 import os
-import sqlalchemy
 from flask_cors import CORS
-from google.cloud import pubsub_v1, storage
 import json
 from datetime import datetime
 # mixdown stuff
@@ -44,25 +42,4 @@ def test_mixdown():
     else:
         return jsonify({"error": "Invalid data"}), 400
 
-# Database connection setup using Cloud SQL Auth Proxy
-def init_db_connection():
-    db_user = "db_user"
-    db_pass = os.getenv("DB_PASS")
-    db_name = "level_values_db"
-    db_host = "127.0.0.1"  # Cloud SQL Auth Proxy runs on localhost
-
-    pool = sqlalchemy.create_engine(
-        sqlalchemy.engine.url.URL(
-            drivername="postgresql+pg8000",
-            username=db_user,
-            password=db_pass,
-            database=db_name,
-            host=db_host,
-            port=5432,  # Default port for PostgreSQL
-            query={}
-        )
-    )
-    return pool
-
-db = init_db_connection()
 app.run(debug=True)
